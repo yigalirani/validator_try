2a2478f2d5c75daec0ade1dfdd936fc453e37e07
Yigal Irani <yigal@symbolclick.com>
2025-11-10 15:30:25 -0500

    added validateuser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import path from 'node:path';
 import {readFile,glob} from 'node:fs/promises';
+import type {User} from './my_types.ts'
+import {Ajv} from "ajv";
+import schema from "../generated/my_types.schema.json" with { type: "json" };
 
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+
+export function validateUser(data: unknown): data is User {
+  const valid = validate(data);
+  if (!valid) console.error(validate.errors);
+  return !!valid;
+}
--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,1 +1,2 @@
-export {};
+import type { User } from './my_types.ts';
+export declare function validateUser(data: unknown): data is User;
